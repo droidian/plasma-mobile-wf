0bb0d1d446d5bf9cc8e7bdc41256258ec0fcdc97
Alexander Rutz <alex@familyrutz.com>
2025-10-24 15:58:29 -0600

    sensorsplugin: add new sensor plugin
    This introduces a new SensorsPlugin for reading sensor values. Currently,
    it uses the proximity sensor to automatically turn the screen off or on
    during calls.

---
+++ components/sensorsplugin/sensors.cpp
@@ -0,0 +1,22 @@
+// SPDX-FileCopyrightText: 2024 Alexander Rutz <arpio@droidian.org>
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#include <QDebug>
+#include <QProximitySensor>
+#include <QProximityReading>
+
+#include "sensors.h"
+
+Sensors::Sensors(QObject *parent)
+    : QObject{parent}
+{
+    QProximitySensor *proxSensor = new QProximitySensor(this);
+    connect(proxSensor, &QProximitySensor::readingChanged, this, [=]() {
+        QProximityReading *reading = proxSensor->reading();
+        if (reading) {
+            qDebug()<<"sensors: "<<reading->close();
+            emit proximityChanged(reading->close());
+        }
+    });
+    proxSensor->start();
+}
